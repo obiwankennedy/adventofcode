0e620fa4ed65ef8ba824a5c7205dda15b30bb5a9
Renaud G <renaud@rolisteam.org>
2017-12-06 08:12:44 +0100

    day 6

--- adventofCode.py
+++ adventofCode.py
@@ -729,13 +729,127 @@ while (pos<size):
 print("Solution: "+str(count))
 print("Exercice 2")
 
-pos = 0
-size=len(instruction2)
-count=0
-while (pos<size):
-    old=pos
-    pos+=instruction2[pos]
-    instruction2[old]+=1 if instruction2[old]<3 else -1
-    count+=1
+#pos = 0
+#size=len(instruction2)
+#count=0
+#while (pos<size):
+#    old=pos
+#    pos+=instruction2[pos]
+#    instruction2[old]+=1 if instruction2[old]<3 else -1
+#    count+=1
 
 print("Solution: "+str(count))
+
+
+
+
+
+print("---")
+print("Day 6")
+
+
+print("Exercice 1")
+currentInput = [5,	1,	10,	0,	1,	7,	13,	14,	3,	12,	8,	10,	7,	12,	0,	6]
+
+def contains(list1,matrix):
+    for list in matrix:
+        if( list1 == list):
+            print(str(list)+"\n"+str(list1))
+            return True
+    return False
+
+previousStep = []
+count =0
+while (not contains(currentInput,previousStep)):
+    count+=1
+    previousStep.append(list(currentInput))
+    maxVal = max(currentInput)
+    index = currentInput.index(maxVal)
+    currentInput[index]=0
+    index+=1
+    while (maxVal > 0):
+        if(index>=len(currentInput)):
+            index=0
+        currentInput[index]+=1
+        maxVal-=1
+        index+=1
+
+print("Solution: "+str(count))
+
+
+
+
+
+print("Exercice 2")
+currentInput = [5,	1,	10,	0,	1,	7,	13,	14,	3,	12,	8,	10,	7,	12,	0,	6]
+
+def contains(list1,matrix):
+    for list in matrix:
+        if( list1 == list):
+            print(str(list)+"\n"+str(list1))
+            return True
+    return False
+
+previousStep = []
+count =0
+while (not contains(currentInput,previousStep)):
+    count+=1
+    previousStep.append(list(currentInput))
+
+    maxVal = max(currentInput)
+    index = currentInput.index(maxVal)
+    currentInput[index]=0
+    index+=1
+
+    while (maxVal > 0):
+        if(index>=len(currentInput)):
+            index=0
+        currentInput[index]+=1
+        maxVal-=1
+        index+=1
+
+
+bigIndex= previousStep.index(currentInput)
+size = len(previousStep)
+count = size-bigIndex
+
+print("Solution: "+str(count))
+
+print("---")
+print("Day 6")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
